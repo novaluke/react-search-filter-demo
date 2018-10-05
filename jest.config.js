90e1b116f85e275ec5bac83c0576fe1b96b4e267
Luke Mayhew <luke@webuildlegends.com>
2018-10-05 08:32:40 -0600

    Add Jest with config and an example test

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: "ts-jest",
+  testMatch: ["<rootDir>/test/**/*.(j|t)s?(x)", "<rootDir>/src/**/*.test.(j|t)s?(x)"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^test/(.*)": "<rootDir>/test/$1",
+  },
+  moduleDirectories: ["src", "node_modules"],
+  watchPathIgnorePatterns: ["<rootDir>/dist"],
+  testPathIgnorePatterns: ["<rootDir>/test/helpers/.*"],
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true
+};
---
+++ test/example.ts
@@ -0,0 +1,5 @@
+describe("example test", () => {
+  it("succeeds", () => {
+    expect(true).toBe(true);
+  });
+});

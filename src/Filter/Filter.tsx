56da12d8d9f09e933e6f4045068b0fa24a9fe710
Luke Mayhew <luke@webuildlegends.com>
2018-10-08 10:54:58 -0600

    Refactor Filter for slightly better semantics and documentation

--- src/Filter/Filter.tsx
+++ src/Filter/Filter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaSpinner } from "react-icons/fa";
 import { combineLatest, merge, Observable } from "rxjs";
-import { debounceTime, filter, map, switchMap } from "rxjs/operators";
+import { debounceTime, map, mapTo, partition, switchMap } from "rxjs/operators";
 
 import { componentFromStream, mergeStates } from "../streamHelpers";
 import { fetchResults, Props, State, whenQueryChanges } from "./operators";
@@ -12,36 +12,36 @@ const Filter = componentFromStream((props$: Observable<Props>) => {
     loading: false,
     results: [],
   };
+  // Split a stream in two based on values that pass or fail the predicate
+  // function. Helps to ensure you've covered all cases, compared to defining
+  // the two streams separately. The strange syntax is due to an idosyncracity
+  // in the types of `partition` (https://github.com/ReactiveX/rxjs/issues/2995)
+  const [newQueries$, queryReset$] = partition<string>(
+    // `query => query` would be more concise, but this is better for
+    // readability/comprehensibility (elegance of expression doesn't lead to
+    // better real-world value, but maintainability and extensibility sure do).
+    query => query.length > 0,
+  )(props$.pipe(whenQueryChanges));
   const state$ = merge(
-    // The streams are structured such that state updates are return values from
-    // the stream. Since the XHR stream requires debouncing, we can't just throw
-    // the loading state update on the front of it, or it will be debounced as
-    // well. Instead, we emit it in a parallel stream. Note: although it may
-    // seeem like all prop changes are being mapped to the loading state, that's
-    // not actually the case. The props stream has been filtered down to a
-    // specific segment, so only changes to that segment are being mapped, and
-    // it's then recombined with the streams that update based on other
-    // segments, to form a complete picture in the end.
-    props$.pipe(
-      whenQueryChanges,
-      map(query => {
-        const loading = { hasError: false, loading: true };
-        const blank = { hasError: false, loading: false, results: [] };
-        return query ? loading : blank;
-      }),
-    ),
-    props$.pipe(
-      whenQueryChanges,
-      // Only fetch when query is not blank (though for some use cases a blank
-      // query might be allowable - ideally Filter would take a flag for that).
-      // `query => !!query` would be more concise, but this is better for
-      // readability/comprehensibility.
-      filter(query => query.length > 0),
+    queryReset$.pipe(mapTo({ hasError: false, loading: false, results: [] })),
+    // It may seem strange that the stream that emits the loading state is
+    // separate from - and operates independently to - the stream that actually
+    // does the loading. However, the request is debounced, and we don't want to
+    // wait until the request is actually kicked off to let the user know we've
+    // received their request and are working on it - that would give the user
+    // the incorrect indication that nothing is happening despite their input.
+    // So the decision to show a loading indicator is actually based off of
+    // *user input* rather than *application action*, and therefore it makes
+    // sense for it to operate independtly of the request triggering. In this
+    // case, having to program in streams guides the developer into developing
+    // proper coupling.
+    newQueries$.pipe(mapTo({ hasError: false, loading: true })),
+    newQueries$.pipe(
       debounceTime(500),
       // Run the fetch function, which returns a stream, and only use the latest
       // stream to be created, cancelling the previous one (and thus cancelling
-      // its XHR). Note: point free notation could be used here, but being
-      // explicit helps with readability/comprehensibility.
+      // its XHR). Note: point free notation could be used here, but again,
+      // being explicit helps with readability/comprehensibility.
       switchMap(query => fetchResults(query)),
     ),
   ).pipe(mergeStates(initialState));
